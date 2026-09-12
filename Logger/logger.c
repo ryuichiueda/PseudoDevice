96acc250ec1b79ef2325cbe202a6079f172784be
Ryuichi Ueda <ryuichiueda@gmail.com>
2026-09-12 16:11:58 +0900

    Add another

---
+++ Logger/logger.c
@@ -0,0 +1,18 @@
+//SPDX-FileCopyrightText: 2026 Ryuichi Ueda <ryuichiueda@gmail.com>
+//SPDX-License-Identifier: GPL-2.0-only
+#include <linux/module.h>
+
+MODULE_AUTHOR("Ryuichi Ueda");
+MODULE_DESCRIPTION("minimum kernel object");
+MODULE_LICENSE("GPL");
+
+char* msg = "module [logger.o]";
+
+int init_module(void) {
+    printk( KERN_INFO "%s : loaded into kernel\n", msg );
+    return 0;
+}
+
+void cleanup_module(void) {
+    printk( KERN_INFO "%s : removed from kernel\n", msg );
+}
